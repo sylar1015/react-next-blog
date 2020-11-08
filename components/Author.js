8a0e888aa777e940424ab7ff66e2d805f87b0bcf
sylar <sylar1015@163.com>
2020-11-08 22:14:37 +0800

    add github link

--- components/Author.js
+++ components/Author.js
@@ -8,11 +8,19 @@ const Author = () => {
         <div className="author-div comm-box">
             <div> <Avatar size={100} src="" /></div>
             <div className="author-introduction">
-                光头程序员，专注于WEB和移动前端开发。要录1000集免费前端视频的傻X。此地维权无门，此时无能为力，此心随波逐流。
+                自我介绍...未完待续...
                 <Divider>社交账号</Divider>
-                <Avatar size={28} className="account" ><GithubOutlined /></Avatar>
-                <Avatar size={28} className="account" ><QqOutlined /></Avatar>
-                <Avatar size={28} className="account" ><WechatOutlined /></Avatar>
+                <Avatar size={28} className="account" >
+                    <a href="https://github.com/sylar1015" target="_blank" title="https://github.com/sylar1015">
+                        <GithubOutlined />
+                    </a>
+                </Avatar>
+                <Avatar size={28} className="account" >
+                    <QqOutlined />
+                </Avatar>
+                <Avatar size={28} className="account" >
+                    <WechatOutlined />
+                </Avatar>
 
             </div>
         </div>
